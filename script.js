421e20d4502891408008b680a035e5579f3de1f5
WlunderGames <wlundergames@gmail.com>
2026-05-28 11:43:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -95,11 +95,11 @@ const tl3 = gsap.timeline({
 });
 tl.to(first_title, {
     opacity: 0,
-    x: "-=5%",
+    fontSize: "-=50px",
     duration: 1,
 }).to(second_title, {
     opacity: 0,
-    x: "+=5%",
+    fontSize: "-=50px",
     duration: 1,
 });
 tl2.to(text_title, {
